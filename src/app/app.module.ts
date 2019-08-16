27ef62e9d005c20c56e9e93ac4c05f752afca769
Mykhailo <mcmikius@icloud.com>
2019-08-16 09:36:02 +0300

    add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,6 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    CoreModule,
+    SharedModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: 'AIzaSyAT2FgoElvecx5tA8o1oRqxTzBLUnisaH4',
+  authDomain: 'ng-blog-itea-finale-project.firebaseio.com',
+  databaseURL: 'https://ng-blog-itea-finale-project.firebaseio.com',
+  projectId: 'ng-blog-itea-finale-project',
+  storageBucket: 'ng-blog-itea-finale-project.appspot.com',
+  messagingSenderId: '780116723089'
+}
 };
 
 /*
